097ca5d3d08af7a71682f4cb06c520ec18b97587
Luis Dominguez <l.dominguez.aldana@gmail.com>
2021-08-24 21:34:14 -0500

    Access index directly /

--- happy/urls.py
+++ happy/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 app_name = "happy"
 urlpatterns = [
-    path("index", views.index, name="index"),
+    path("", views.index, name="index"),
     path("single_product/<int:id>", views.single_product, name="single_product")
 ]
